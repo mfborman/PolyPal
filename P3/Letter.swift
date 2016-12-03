2eb5ad56278255f7703cf2e8c64b8ad3883c7751
mfborman <mfborman@iastate.edu>
2016-12-03 17:57:08 -0600

    Added class file for letters in alphabet game

---
+++ P3/Letter.swift
@@ -0,0 +1,13 @@
+//
+//  Letter.swift
+//  P3
+//
+//  Created by Mitchell Borman on 12/3/16.
+//  Copyright © 2016 Tony Branson. All rights reserved.
+//
+
+import UIKit
+
+class Letter: SKSpriteNode {
+
+}
