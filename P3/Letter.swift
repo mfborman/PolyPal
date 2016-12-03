a810353d7b95b4dca8e92d42a9dd126446b9d9f2
mfborman <mfborman@iastate.edu>
2016-12-03 17:59:13 -0600

    Hoping to retrieve letter class

--- P3/Letter.swift
+++ P3/Letter.swift
@@ -7,7 +7,46 @@
 //
 
 import UIKit
+import SpriteKit
 
 class Letter: SKSpriteNode {
+    
+    let image: SKTexture
+    let letterImageName: String
+    let letterName: String
+    
+    init(letterName: String) {
+        
+        self.letterName = letterName.components(separatedBy: ".")
+        letterImageName = letterName[1]
+
+        // Resize sprite based on screen size
+        let spriteSizeRatio = sprite.size.height/sprite.size.width
+        var spriteSize: CGSize = screenSize
+        spriteSize.height = screenSize.height * 0.1//changed from 0.2 to 0.09 because more letters
+        spriteSize.width = spriteSize.height * spriteSizeRatio//this will shrink also
+        sprite.size = spriteSize
+        
+        let xOffset : CGFloat
+        let yOffset : CGFloat
+        if i < 13 {
+            yOffset = 0.8
+            xOffset = CGFloat(i+1) / 14.0
+            
+        } else {
+            yOffset = 0.65
+            xOffset = CGFloat(i-12) / 14.0
+        }
+        let push = size.width * (1/14)
+        let x = push + (size.width * (12/14) * xOffset)
+        sprite.position = CGPoint(x: x, y: size.height * yOffset)
+        sprite.zPosition = 2.0
+        
+        super.init(texture: nil, color: .clear, size: spriteSize)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
 
 }
--- README.txt
+++ README.txt
@@ -1,17 +1,14 @@
 Format for GIT commit comments:
 
-"Short 10 or less word description"
+  "Short 10 or less word description"
 
-In file "filename"
-  - "Update1"
-  - "Update2"
-  - ...
-  - "UpdateN"
+  In file "filename"
+    - "Update1"
+    - "Update2"
+    - ...
 
-In file "filename2"
-  - "Update1"
-  - "Update2"
-  - ...
-  - "UpdateN"
-
-In file "filenameN"
+How to edit the .gitignore file
+  1. Navigate to the project home directory
+  2. Run the command: mv .gitignore gitignore.txt
+  3. Edit the file with: vim gitignore.txt
+  4. After saving the file, run: mv gitignore.txt .gitignore
